3efabe740f262332f1579d9cc7e54f658140b1ca
Vishal Patel <vishalpatel2890@gmail.com>
2017-08-15 18:47:50 -0400

    added access control headers

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -62,7 +62,7 @@ export function fetchRecipe(url) {
           'X-Requested-With': 'XMLHttpRequest',
           'Access-Control-Allow-Origin': '*',
           // 'Access-Control-Allow-Credentials': 'True',
-          'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+          'Access-Control-Allow-Headers': 'accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with',
           'Access-Control-Allow-Methods' : 'GET, OPTIONS'
       },
     })
