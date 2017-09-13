a91758ac87362603d05c32d033ab01ea6c7a6cc2
Vishal <vishalpatel2890@gmail.com>
2017-09-13 08:40:35 -0400

    reducer cleanup

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -2,35 +2,15 @@ import {
   RECEIVE_RECIPES,
   RECEIVE_STEPS,
   RECEIVE_INGREDIENTS,
-<<<<<<< HEAD
-  HAS_ERRORED,
-  DELETE_SUCCESS
-} from '../constants/appConstants'
-
-// const initialState = {
-//     isLoading : true,
-//     recipe : []
-// };
-
-export function recipe(state = [], action) {
-=======
   RECIPE_HAS_ERRORED,
   DELETE_SUCCESS,
   ADD_RECIPE
 } from '../constants/appConstants'
 
 export const recipe = (state = [], action) => {
->>>>>>> development
   switch (action.type) {
     case RECEIVE_RECIPES:
       return [...state, ...action.json]
-<<<<<<< HEAD
-
-    case DELETE_SUCCESS:
-      return [...state, ...action.payload]
-
-    case HAS_ERRORED:
-=======
 
     case ADD_RECIPE:
       return [state, action.file]
@@ -42,7 +22,6 @@ export const recipe = (state = [], action) => {
       ]
 
     case RECIPE_HAS_ERRORED:
->>>>>>> development
       return action.hasErrored
 
     default:
@@ -50,11 +29,7 @@ export const recipe = (state = [], action) => {
   }
 }
 
-<<<<<<< HEAD
-export function ingredients(state = [], action) {
-=======
 export const ingredients = (state = [], action) => {
->>>>>>> development
   switch (action.type) {
     // case REQUEST_RECIPES:
     //   return [state, action.isLoading];
@@ -67,11 +42,7 @@ export const ingredients = (state = [], action) => {
   }
 }
 
-<<<<<<< HEAD
-export function steps(state = [], action) {
-=======
 export const steps = (state = [], action) => {
->>>>>>> development
   switch (action.type) {
     // case REQUEST_RECIPES:
     //   return [state, action.isLoading];
